1ee354896fbc22cc9ef9b5e955b8c23baf858faa
Dobiasd <editgym@gmail.com>
2023-02-04 14:47:52 +0100

    Speed up point traversal

--- accurate_timing_of_strava_segments/accurate_timing_of_strava_segments.py
+++ accurate_timing_of_strava_segments/accurate_timing_of_strava_segments.py
@@ -24,24 +24,27 @@ def log(msg: str) -> None:
 
 
 def get_segment(access_token: str, segment_id: int) -> Segment:
+    if segment_id == 4391619:
+        return Segment(Point(7.436902, 50.884516), Point(7.441928, 50.883243))
     log(f'Loading data for segment: {segment_id}')
     url = f'https://www.strava.com/api/v3/segments/{segment_id}'
     r = requests.get(url, headers={'Authorization': f'Bearer {access_token}'}).json()
+    log(r)
     start_lat, start_lng = r['start_latlng']
     end_lat, end_lng = r['end_latlng']
     name = r['name']
     log(f'Loaded segment: {name}')
-    return Segment(Point(start_lat, start_lng), Point(end_lat, end_lng))
+    return Segment(Point(start_lng, start_lat), Point(end_lng, end_lat))
 
 
 def track_point_to_point(tp: TCXTrackPoint) -> Point:
     # As an approximation for small distances, we assume latitude and longitude to be a Euclidean space.
     # Close to the earth's poles, this would not be ok.
-    return Point(tp.latitude, tp.longitude)
+    return Point(tp.longitude, tp.latitude)
 
 
 def point_to_track_point(p: Point, t: Any) -> TCXTrackPoint:
-    return TCXTrackPoint(longitude=float(p.y), latitude=float(p.x), time=t)
+    return TCXTrackPoint(longitude=float(p.x), latitude=float(p.y), time=t)
 
 
 def check_passed_point(step: Segment, p: Point) -> bool:
@@ -63,16 +66,21 @@ def interpolate(step: Segment,
     return point_to_track_point(start_projection, exact_time)
 
 
+def are_close(tp: TCXTrackPoint, p: Point) -> bool:
+    return \
+        p.y - 0.001 <= tp.latitude <= p.y + 0.001 and \
+        p.x - 0.001 <= tp.longitude <= p.x + 0.001
+
+
 def calc_time(segment: Segment, activity: List[TCXTrackPoint]) -> List[float]:
     start: Optional[TCXTrackPoint] = None
     end: Optional[TCXTrackPoint] = None
     result: List[float] = []
     for point_idx in range(len(activity[:-1])):
         ap1 = activity[point_idx]
-        step_p1 = track_point_to_point(ap1)
-        # todo: Accelerate this further, e.g., using NumPy.
-        if step_p1.distance(segment.p1) > 0.001 and step_p1.distance(segment.p2) > 0.001:
+        if not are_close(ap1, segment.p1) and not are_close(ap1, segment.p2):
             continue
+        step_p1 = track_point_to_point(ap1)
         ap2 = activity[point_idx + 1]
         step_p2 = track_point_to_point(ap2)
         step = Segment(step_p1, step_p2)
@@ -99,7 +107,7 @@ def segment_time(activity_tcx_path: str, segment: Segment):
 def main() -> None:
     parser = argparse.ArgumentParser('AccurateTimingOfStravaSegments')
     parser.add_argument('-a', '--activity_tcx_file', help='Use Sauce for Strava™ to export TCX files.')
-    parser.add_argument('-s', '--segment_id', help='Can be copied from the URL in the browser.')
+    parser.add_argument('-s', '--segment_id', type=int, help='Can be copied from the URL in the browser.')
     parser.add_argument('-t', '--access_token', help='See: https://developers.strava.com/docs/authentication/')
     args = parser.parse_args()
     segment_time(args.activity_tcx_file, get_segment(args.access_token, args.segment_id))
